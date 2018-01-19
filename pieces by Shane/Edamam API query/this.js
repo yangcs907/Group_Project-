27ceef7e66a0bfce6a4f02cdd617a69992d9abc5
Shane Scott <shaneradio@gmail.com>
2018-01-18 20:55:53 -0500

    API query stuff

---
+++ pieces by Shane/Edamam API query/this.js
@@ -0,0 +1,20 @@
+
+
+
+$(document).on("click", "#callButton", function (){
+
+  $("#divID").empty();
+
+  var queryTerm = $(this).data("searchTerm");
+
+// This next bit lacks a small structural element, yet. It seems a matter of HTML or CSS, really. This is the in-window placement of the associated rating in relation to each GIF image.
+
+  $.ajax({
+        url: "https://api.edamam.com/search?q=" + queryTerm + "&api_key=" + apiKey + "&limit=10",
+        method: "get"
+      })
+
+      .done(function(response) {
+
+        console.log(response);
+      };
