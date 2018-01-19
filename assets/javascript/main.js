55f98b28df3747b1652ee57508ef9d52e7a305a8
Chris Yang <chrisyang@lawn-128-61-60-83.lawn.gatech.edu>
2018-01-18 20:06:01 -0500

    added firebase config

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC8tE8l_DBoyPfFaiRc5fYfZ6jBK9XrcSs",
+    authDomain: "groupproject-1b84f.firebaseapp.com",
+    databaseURL: "https://groupproject-1b84f.firebaseio.com",
+    projectId: "groupproject-1b84f",
+    storageBucket: "",
+    messagingSenderId: "208222818748"
+  };
+  firebase.initializeApp(config);
