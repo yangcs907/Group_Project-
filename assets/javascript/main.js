a2a4f39d6599b0991cda0de967eda962b7598b61
Chris Yang <chrisyang@lawn-128-61-60-83.lawn.gatech.edu>
2018-01-18 21:34:07 -0500

    update js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,26 @@
-var ingredient_1 = $("").val().trim();
-var ingredient_2 = $("").val().trim();
-var ingredient_3 = $("").val().trim();
-var ingredient_4 = $("").val().trim();
-var ingredient_5 = $("").val().trim();
 
-function searchRecipes(ingredients) {
-  var queryURL = ""
+function searchRecipes(one, two, three, four, five) {
+  var queryURL = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${015a1dc7}&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function response) {
+  }).done(function(response) {
+    console.log(response);
 
 
   }
-}
+};
 
+$("").on("click", function(event) {
+  event.preventDefault();
+  var ingredient_1 = $("").val().trim();
+  var ingredient_2 = $("").val().trim();
+  var ingredient_3 = $("").val().trim();
+  var ingredient_4 = $("").val().trim();
+  var ingredient_5 = $("").val().trim();
+
+  searchRecipes(ingredient_1, ingredient_2, ingredient_3, ingredient_4, ingredient_5);
+});
 
 
   // Initialize Firebase
@@ -27,3 +33,5 @@ function searchRecipes(ingredients) {
     messagingSenderId: "208222818748"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
