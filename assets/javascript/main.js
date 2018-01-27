f7ac9bbda4da90438724d45f42728d17e569cee6
Shane Scott <shaneradio@gmail.com>
2018-01-26 19:55:44 -0500

    latest js from me

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,56 +1,74 @@
 $(document).ready(function() {
   // <script src="Chart.bundle.min.js"></script>
 
-  $('#findIngr').on('click', function(event) {
-    // event.preventDefault() can be used to prevent an event's default behavior.
-    // Here, it prevents the submit button from trying to submit a form when clicked
-    event.preventDefault();
-    // Here we grab the text from the input box
-    var ingredient = $('#ingr-input')
-      .val()
-      .trim();
+  var newestData;
 
-    var appId = '015a1dc7';
-    // "36c66d3a"
+  function removeData(chart) {
+        chart.data.labels.pop();
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        chart.update();
+  };
 
-    var appKey = '16bab94aee14b5881f28798e788c26e0';
-    // "8be8dd8b6a6f98a5221770fcb1d2f043"
-    // Here we construct our URL
-    var queryURL =
-      'https://api.edamam.com/search?q=' +
-      ingredient +
-      '&app_id=' +
-      appId +
-      '&app_key=' +
-      appKey +
-      '&from=0&to=1';
-    // https://api.edamam.com/search?app_id=yourappid&app_key=yourappkey&q=butter&diet=low-carb&diet=high-protein
+  function addData(chart, label, data) {
+      chart.data.labels.push(label);
+      chart.data.datasets.forEach((dataset) => {
+          dataset.data.push(data);
+      });
+      chart.update();
+  };
 
-    var retrData;
+  function updateChart() {
+      var newestData = [retrCarbData, retrProData, retrFatData, retrSatFatData, retrFiberData];
 
-    function saveLocal(z) {
-      console.log(z);
-      localStorage.setItem('ourData', JSON.stringify(z));
+      var ourLabels = ["Carbs", "Protein", "Total Fat", "Saturated Fat", "Fiber"];
+      
+      ourLabels.forEach(function(label){
+        removeData(theChart);
+        });
+
+      // for (j = 0; j < ourLabels.length; j++) {
+      //   removeData(theChart);
+      // };
+
+      for (i = 0; i < ourLabels.length; i++) {
+        addData (theChart, ourLabels[i], newestData[i]);
+      };
+
+    };
+
+    function saveLocal(resp) {
+      console.log(resp);
+      localStorage.setItem('ourData', JSON.stringify(resp));
       localStorage.setItem(
         'carbDataLS',
-        JSON.stringify(z.hits[0].recipe.totalDaily.CHOCDF.quantity)
+        JSON.stringify(resp.hits[0].recipe.totalDaily.CHOCDF.quantity)
       );
       localStorage.setItem(
         'proDataLS',
-        JSON.stringify(z.hits[0].recipe.totalDaily.PROCNT.quantity)
+        JSON.stringify(resp.hits[0].recipe.totalDaily.PROCNT.quantity)
       );
       localStorage.setItem(
         'fatDataLS',
-        JSON.stringify(z.hits[0].recipe.totalDaily.FAT.quantity)
+        JSON.stringify(resp.hits[0].recipe.totalDaily.FAT.quantity)
       );
       localStorage.setItem(
         'satFatDataLS',
-        JSON.stringify(z.hits[0].recipe.totalDaily.FASAT.quantity)
+        JSON.stringify(resp.hits[0].recipe.totalDaily.FASAT.quantity)
       );
       localStorage.setItem(
         'fiberDataLS',
-        JSON.stringify(z.hits[0].recipe.totalDaily.FIBTG.quantity)
-      );
+        JSON.stringify(resp.hits[0].recipe.totalDaily.FIBTG.quantity));
+
+        // theChart.data.datasets.data = [retrCarbData, retrProData, retrFatData, retrSatFatData,retrFiberData];
+      // newestData = [];
+      // theChart.data.datasets.data = newestData
+
+      // theChart.config.data =  (theChart.data.datasets.data)
+      // theChart.update ();
+      // console.log(theChart.data.datasets.data);
+      
 
       retrData = JSON.parse(localStorage.getItem('ourData'));
 
@@ -96,7 +114,40 @@ $(document).ready(function() {
       $('#thisHere').append('Total Fat: ' + fatData);
       $('#thisHere').append('Saturated Fat: ' + satFatData);
       $('#thisHere').append('Fiber: ' + fiberData);
-    }
+
+      updateChart();
+    };
+
+  $('#findIngr').on('click', function(event) {
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    $("#thisHere").empty();
+    // Here we grab the text from the input box
+    var ingredient = $('#ingr-input')
+      .val()
+      .trim();
+
+    var appId = '015a1dc7';
+    // "36c66d3a"
+
+    var appKey = '16bab94aee14b5881f28798e788c26e0';
+    // "8be8dd8b6a6f98a5221770fcb1d2f043"
+    // Here we construct our URL
+    var queryURL =
+      'https://api.edamam.com/search?q=' +
+      ingredient +
+      '&app_id=' +
+      appId +
+      '&app_key=' +
+      appKey +
+      '&from=0&to=1';
+    // https://api.edamam.com/search?app_id=yourappid&app_key=yourappkey&q=butter&diet=low-carb&diet=high-protein
+
+    var retrData;
+
+    // theChart.update ();
 
     // =====================================================
     // =====================================================
@@ -171,8 +222,39 @@ $(document).ready(function() {
         ]
       }
     }
+
+
+
   });
 
-  theChart.data.datasets.data = [retrCarbData, retrProData, retrFatData, retrSatFatData,retrFiberData];
-      theChart.update();
+
+
+  // removeData(theChart);
+  // removeData(theChart);
+  // removeData(theChart);
+  // removeData(theChart);
+  // removeData(theChart);
+
+
+  // ourLabels.forEach(addData (theChart, index?)){
+
+  // };
+
+  // addData(theChart, "Carbs", retrCarbData);
+  // addData(theChart, "Protein", retrProData);
+  // addData(theChart, "Total Fat", retrFatData);
+  // addData(theChart, "Saturated Fat", retrSatFatData);
+  // addData(theChart, "Fiber", retrFiberData);
+
+
+// for (i = 0; i < ourLabels.length; i++) {
+//   addData (theChart, ourLabels[i], newestData[i]);
+// };
+
+  
+
+//   addData(theChart, ourLabels, newestData);
+
+  // theChart.update();
+
 });
