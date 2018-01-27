ff94c614894cdc53a64fd13ea0e87a7b11ca0334
Tyler Hunter <tyler.a.hunter@gmail.com>
2018-01-27 03:44:12 -0500

    recipe opens in new tab

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -55,7 +55,7 @@
 
             //Placing results in DOM
             // Creates a div to hold the recipe
-            var recipeCard = $('<a href=' + recipe_instr + '<div class="card results col-sm-3 col-lg-3 col-md-3" >'+
+            var recipeCard = $('<a href=' + recipe_instr + ' + target="_blank" <div class="card results col-sm-3 col-lg-3 col-md-3" >'+
                                   '<img class="card-img-top" src=' + recipe_img + ' alt="Card image cap">'
                                  +'<div class="card-body">' +
                                     '<h5 class="card-title">' + recipe_title + '</h5>' +
@@ -74,20 +74,23 @@
             
             //Recipe instruction link
             var recipe_instr = response.hits[x].recipe.url;
-            
+
+
             //Placing results in DOM
             // Creates a div to hold the recipe
-            var recipeCard = $('<a href=' + recipe_instr + '<div class="card results col-sm-3 col-lg-3 col-md-3" >'+ 
+            var recipeCard = $('<a href="+ recipe_instr +"+ target="_blank" <div class="card results col-sm-3 col-lg-3 col-md-3" >'+
                                   '<img class="card-img-top" src=' + recipe_img + ' alt="Card image cap">'
                                  +'<div class="card-body">' + 
                                     '<h5 class="card-title">' + recipe_title + '</h5>' + 
                                   '</div>' + 
                                 '</div>' + '</a>');
+            window.open(recipeCard,'_blank');
            
             $("#deck-" + deckNumber).append(recipeCard); 
             
             //Add the src attr for recipe instructions url on each card
              recipeCard.attr('src', recipe_instr);
+             recipeCard.attr('target','_blank');
              recipeCard.addClass("food");
              recipeCard.attr("data-instr", "recipe");
 
